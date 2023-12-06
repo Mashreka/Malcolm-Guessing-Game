092baa0b9e1effa02af74c1bb2f6a00a9a651b93
Mashreka <mashreka.mannan@gmail.com>
2023-12-06 12:12:12 +0100

    *renamed variable för att följa Javas namnkonventioner. *Förenklade logiken i checkGuess och playAgain metoder genom att direkt returnera resultatet istället för att använda en separat variabel.

--- src/Main.java
+++ src/Main.java
@@ -9,51 +9,45 @@ public class Main {
 
         while (continueGame) {                                                          //en loop som kör programmet så länge continueGame är true
             int randomNumber = random.nextInt(100)+1;                           //Generar ett slumpmässigt heltal och sparar i variabeln "randomNumber"
-            System.out.println("Gissa på ett nummer mellan 1 - 100 ");                //Printar ut på skärmen och ber användaren välja ett nummer mellan 1-100
-            int Tries = 0;                                                           //sparar en variabel som heter "Tries" som börjar på 0 som ska ta emot antalen gissningar användaren har gissat
+            System.out.println("Gissa på ett nummer mellan 1 - 100 ");//Printar ut på skärmen och ber användaren välja ett nummer mellan 1-100
+           // renamed variable för att följa Javas namnkonventioner. Variabelnamn bör börja med en liten bokstav
+            int tries = 0;                                                          //sparar en variabel som heter "Tries" som börjar på 0 som ska ta emot antalen gissningar användaren har gissat
             boolean correctGuess = false;                                           //En boolean som kontrollerar om rätt svar har gissats och isåfall avslutar loopen
 
             while (!correctGuess) {
-                int Guess = scanner.nextInt();                                       //Scannern sparar användarens input i integern "Guess"
-                Tries++;                                                            //för var gång som loopen körs om plusas antalet försök på med 1
+                // renamed variable
+                int guess = scanner.nextInt();                                       //Scannern sparar användarens input i integern "Guess"
+                tries++;                                                            //för var gång som loopen körs om plusas antalet försök på med 1
 
-                if (checkGuess(randomNumber,Guess)==0) {                            //kontrollerar om användarens gissning är rätt
+                if (checkGuess(randomNumber,guess)==0) {                            //kontrollerar om användarens gissning är rätt
                     correctGuess = true;                                           //När användaren har gissat rätt har jag lagt en break här som avslutar inre while loop genom att sätta correctGuess värde till true
                     System.out.println("Det var rätt, Bra gissat du! ");
-                    System.out.println("Det tog dig totalt " + Tries + " Försök"); //En print som berättar hur många försök det krävdes för användaren att gissa rätt
+                    System.out.println("Det tog dig totalt " + tries + " Försök"); //En print som berättar hur många försök det krävdes för användaren att gissa rätt
                 }
-                else if (checkGuess(Guess,randomNumber)==-2) {
+                else if (checkGuess(guess,randomNumber)==-2) {
                     System.out.println("FEL! Talet är högre än Försök igen.  ");
                 } else {
                     System.out.println("FEL! Talet är lägre än försök igen.");
                 }
             }
             System.out.println("Vill du spela igen? (Ja/Nej)");
-            String Answer = scanner.next();
-            continueGame = playAgain(Answer);                                       // Anropar "playAgain" -metoden för att avgöra om spelet ska fortsätta.
+            // renamed variable
+            String answer = scanner.next();
+            continueGame = playAgain(answer);                                       // Anropar "playAgain" -metoden för att avgöra om spelet ska fortsätta.
         }
     }
-
-    public static int checkGuess(int randomNumber, int guess){
-        int value = 0;
-
-        if (guess == randomNumber){
-            value = 0;                                                              // Gissningen är korrekt.
+    //Förenklade logiken i checkGuess-metoden genom att direkt returnera resultatet istället för att använda en separat variabel.
+    public static int checkGuess(int randomNumber, int guess) {
+        if (guess == randomNumber) {
+            return 0; // Correct guess
+        } else if (guess < randomNumber) {
+            return -1; // Guess is lower than the random number
+        } else {
+            return -2; // Guess is higher than the random number
         }
-        else if (guess < randomNumber){
-            value =-1;                                                              // Gissningen är lägre än det slumpmässiga talet.
-        }
-        else if (guess > randomNumber) {
-            value =-2;                                                              // Gissningen är högre än det slumpmässiga talet.
-        }
-        return (value);                                                             // Returnerar resultatet av gissningen.
     }
-    public static boolean playAgain(String Answer) { //En metod som frågar om användaren vill spela igen
-        boolean btn = true;
-        if(Answer.equalsIgnoreCase("Nej")){
-            return (false);
-        }
-        return (btn);
-
+    //Förenklade playAgain-metoden genom att direkt returnera resultatet av jämförelsen istället för att använda en separat variabel.
+    public static boolean playAgain(String answer) {
+        return answer.equalsIgnoreCase("Ja");
     }
 }
---
+++ src/MainGUI.java
@@ -0,0 +1,123 @@
+//Vidareutvecklingen av projektet
+import java.util.Random;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainGUI {
+    private static Random random = new Random();
+    private static int randomNumber;
+    private static int tries;
+    private static JFrame frame;
+    private static JPanel panel;
+    private static JLabel label;
+    private static JTextField textField;
+    private static JButton button;
+    private static JButton yesButton;
+    private static JButton noButton;
+
+    public static void main(String[] args) {
+        initializeGUI(); //calls the initializeGUI method to set up the graphical user interface
+        initializeGame(); //calls the initializeGame method to initialize the game variables
+
+        /*
+           when button is clicked, it retrieves the user's guess from the text field,
+           increments the number of tries,
+           and checks the guess against the random number.
+           Depending on the result, it displays the appropriate message
+           using the showGameOverMessage or showWrongGuessMessage methods.
+         */
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int guess = Integer.parseInt(textField.getText());
+                tries++;
+
+                if (checkGuessResult(randomNumber, guess) == 0) {
+                    showGameOverMessage("Congratulations! You guessed it right.\nIt took you " + tries + " tries.\nDo you want to play again?");
+                } else if (checkGuessResult(guess, randomNumber) == -2) {
+                    showWrongGuessMessage("Wrong guess! The number is higher. Try again.");
+                } else {
+                    showWrongGuessMessage("Wrong guess! The number is lower. Try again.");
+                }
+            }
+        });
+        /*
+            when the buttons are clicked,
+            yesButton resets the game by calling the resetGame method,
+            while the noButton exits the program.
+         */
+
+        yesButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                resetGame();
+            }
+        });
+
+        noButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+    //initializeGUI method sets up the graphical user interface
+    public static void initializeGUI() {
+        frame = new JFrame("Guessing Game");
+        panel = new JPanel();
+        label = new JLabel("Guess a number between 1 - 100: ");
+        textField = new JTextField(10);
+        button = new JButton("Guess");
+        yesButton = new JButton("Yes");
+        noButton = new JButton("No");
+
+        panel.add(label);
+        panel.add(textField);
+        panel.add(button);
+
+        frame.add(panel);
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    //initializeGame method initializes the game by generating a random number
+    public static void initializeGame() {
+        randomNumber = random.nextInt(100) + 1;
+        tries = 0;
+    }
+    /*
+         checkGuessResult method compares the user's guess with the random number
+         and returns an integer value indicating the result
+     */
+    public static int checkGuessResult(int randomNumber, int guess) {
+        if (guess == randomNumber) {
+            return 0; // Correct guess
+        } else if (guess < randomNumber) {
+            return -1; // Guess is lower than the random number
+        } else {
+            return -2; // Guess is higher than the random number
+        }
+    }
+    // showGameOverMessage and showWrongGuessMessage methods display a message dialog with the provided message using the JOptionPane class.
+    public static void showGameOverMessage(String message) {
+        int playAgain = JOptionPane.showConfirmDialog(frame, message, "Game Over", JOptionPane.YES_NO_OPTION);
+        if (playAgain == JOptionPane.YES_OPTION) {
+            resetGame();
+        } else {
+            System.exit(0);
+        }
+    }
+
+    public static void showWrongGuessMessage(String message) {
+        JOptionPane.showMessageDialog(frame, message);
+    }
+    //resetGame method resets the game by calling the initializeGame method
+    //It also clears the text field by setting its text to an empty string.
+    public static void resetGame() {
+        initializeGame();
+        textField.setText("");
+    }
+}
